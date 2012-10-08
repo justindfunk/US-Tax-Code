a63ab953bffb8c891ffa48bb224b590483904aff
Justin Funk <justindfunk@gmail.com>
2012-10-08 02:52:51 -0400

    Added Title 26 - Subtitle A - Chapter 1 - Subchapter A - Part 1- 1 (c). Abstracted calculator logic

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -1,88 +1,77 @@
 module TaxCalculator
-  
-  def TaxCalculator.Tax(taxreturn)
-    flatamt = 0
-    rate = 0
-    taxableamt = 0
-    # Title 26 > Subtitle A > Chapter 1 > Subchapter A > Part 1 > 1
 
-    # (a) Married individuals filing joint returns and surviving spouses
-    if (taxreturn.type == :marriedjoint || taxreturn.type == :survivingspouse)
-      # If taxable income is not over $36,900
-      if (taxreturn.income <= 36900)
-        # The tax is 15% of taxable income
-        rate = 0.15
-        taxableamt = taxreturn.income
-      end
-      # If taxable income is over $36,900 but not over $89,150
-      if (taxreturn.income > 36900 && taxreturn.income <= 89150)
-        # The tax is $5,535 plus 28% of the excess over $36,900
-        flatamt = 5535
-        taxableamt = taxreturn.income - 36900
-        rate = 0.28
-      end
-      # If taxable income is over $89,150 but not over $140,000
-      if (taxreturn.income > 89150 && taxreturn.income <= 140000)
-        # The tax is $20,165 plus 31% of the excess over $89,150
-        flatamt = 20165
-        taxableamt = taxreturn.income - 89150
-        rate = 0.31
-      end
-      # If taxable income is over $140,000 but not over $250,000
-      if (taxreturn.income > 140000 && taxreturn.income <= 250000)
-        # The tax is $35,928.50 plus 36% of the excess over $140,000
-        flatamt = 35928.50
-        taxableamt = taxreturn.income - 140000
-        rate = 0.36
-      end
-      #If taxable income is over $250,000
-      if (taxreturn.income > 250000)
-        # The tax is $75,528.50 plus 39.6% of the excess over $250,000
-        flatamt = 75528.50
-        taxableamt = taxreturn.income - 250000
-        rate = 0.396
-      end
-    end
-    
-    # (b) Heads of households
-    if (taxreturn.type == :headofhousehold)
-      # If taxable income is not over $29,600
-      if (taxreturn.income <= 29600)
-        # The tax is 15% of taxable income
-        rate = 0.15
-        taxableamt = taxreturn.income
-      end
-      # If taxable income is over $29,600 but not over $76,400
-      if (taxreturn.income > 29600 && taxreturn.income <= 76400)
-        # The tax is $4,440 plus 28% of the excess over $29,600
-        flatamt = 4440
-        taxableamt = taxreturn.income - 29600
-        rate = 0.28
-      end
-      # If taxable income is over $76,400 but not over $127,500
-      if (taxreturn.income > 76400 && taxreturn.income <= 127500)
-        # The tax is $17,544 plus 31% of the excess over $76,400
-        flatamt = 17544
-        taxableamt = taxreturn.income - 76400
-        rate = 0.31
-      end
-      # If taxable income is over $127,500 but not over $250,000
-      if (taxreturn.income > 127500 && taxreturn.income <= 250000)
-        # The tax is $33,385 plus 36% of the excess over $127,500
-        flatamt = 33385
-        taxableamt = taxreturn.income - 127500
-        rate = 0.36
-      end
-      #If taxable income is over $250,000
-      if (taxreturn.income > 250000)
-        # The tax is $77,485 plus 39.6% of the excess over $250,000
-        flatamt = 77485
-        taxableamt = taxreturn.income - 250000
-        rate = 0.396
-      end
-    end
-    
+  BRACKET_THRESHOLD = Hash.new
+  BRACKET_BASE = Hash.new
+  BRACKET_RATE = Hash.new
+  # Title 26 > Subtitle A > Chapter 1 > Subchapter A > Part 1 > 1
+
+  # (a) Married individuals filing joint returns and surviving spouses
+  # If taxable income is not over $36,900 the tax is 15% of taxable income
+  # If taxable income is over $36,900 but not over $89,150 the tax is $5,535 plus 28% of the excess over $36,900
+  # If taxable income is over $89,150 but not over $140,000 the tax is $20,165 plus 31% of the excess over $89,150
+  # If taxable income is over $140,000 but not over $250,000 the tax is $35,928.50 plus 36% of the excess over $140,000
+  # If taxable income is over $250,000 the tax is $75,528.50 plus 39.6% of the excess over $250,000
+  BRACKET_THRESHOLD[:marriedjoint] = [ 36900, 89150, 140000, 250000 ]
+  BRACKET_THRESHOLD[:survivingspouse] = [ 36900, 89150, 140000, 250000 ]
+  BRACKET_BASE[:marriedjoint] = [ 0, 5535, 20165, 35928.50, 75528.50 ]
+  BRACKET_BASE[:survivingspouse] = [ 0, 5535, 20165, 35928.50, 75528.50 ]
+  BRACKET_RATE[:marriedjoint] = [ 0.15, 0.28, 0.31, 0.36, 0.396 ]
+  BRACKET_RATE[:survivingspouse] = [ 0.15, 0.28, 0.31, 0.36, 0.396 ]
+
+  # (b) Heads of households
+  # If taxable income is not over $29,600 the tax is 15% of taxable income
+  # If taxable income is over $29,600 but not over $76,400 the tax is $4,440 plus 28% of the excess over $29,600
+  # If taxable income is over $76,400 but not over $127,500 the tax is $17,544 plus 31% of the excess over $76,400
+  # If taxable income is over $127,500 but not over $250,000 the tax is $33,385 plus 36% of the excess over $127,500
+  # If taxable income is over $250,000 the tax is $77,485 plus 39.6% of the excess over $250,000
+  BRACKET_THRESHOLD[:headofhousehold] = [ 29600, 76400, 127500, 250000 ]
+  BRACKET_BASE[:headofhousehold] = [ 0, 4440, 17544, 33385, 77485 ]
+  BRACKET_RATE[:headofhousehold] = [ 0.15, 0.28, 0.31, 0.36, 0.396 ]
+
+
+  # (c) Unmarried individuals
+  # If taxable income is not over $22,100 the tax is 15% of taxable income
+  # If taxable income is over $22,100 but not over $53,500 the tax is $3315 plus 28% of the excess over $22,100
+  # If taxable income is over $53,500 but not over $115,000 the tax is $12,107 plus 31% of the excess over $53,500
+  # If taxable income is over $115,000 but not over $250,000 the tax is $31,172 plus 36% of the excess over $115,000
+  # If taxable income is over $250,000 the tax is $79772 plus 39.6% of the excess over $250,000
+  BRACKET_THRESHOLD[:unmarried] = [ 22100, 53500, 115000, 250000 ]
+  BRACKET_BASE[:unmarried] = [ 0, 3315, 12107, 31172, 79772 ]
+  BRACKET_RATE[:unmarried] = [ 0.15, 0.28, 0.31, 0.36, 0.396 ]
+
+  def TaxCalculator.Tax(taxreturn)
+    flatamt = GetBaseAmount(taxreturn)
+    rate = GetRate(taxreturn)
+    taxableamt = GetTaxableAmount(taxreturn)
+
     return flatamt + taxableamt * rate
   end
-  
+
+  def TaxCalculator.GetBracket(taxreturn)
+    bracket = 0
+    (0...BRACKET_THRESHOLD[taxreturn.type].count).each do |i|
+      if taxreturn.income > BRACKET_THRESHOLD[taxreturn.type][i]
+        bracket = i+1
+      end
+    end
+    return bracket
+  end
+
+  def TaxCalculator.GetBaseAmount(taxreturn)
+    return BRACKET_BASE[taxreturn.type][GetBracket(taxreturn)]
+  end
+
+  def TaxCalculator.GetRate(taxreturn)
+    return BRACKET_RATE[taxreturn.type][GetBracket(taxreturn)]
+  end
+
+  def TaxCalculator.GetTaxableAmount(taxreturn)
+    bracket = GetBracket(taxreturn)
+    if bracket == 0
+      return taxreturn.income
+    else
+      return taxreturn.income - BRACKET_THRESHOLD[taxreturn.type][bracket - 1]
+    end
+  end
+
 end
